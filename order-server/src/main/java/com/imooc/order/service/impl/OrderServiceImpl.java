84fa57b123c48b280663cc0fbccf8d943eedec03
高亭艳 <gaoty1@guahao.com>
2019-04-23 16:02:39 +0800

    order 初始化

---
+++ order-server/src/main/java/com/imooc/order/service/impl/OrderServiceImpl.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2001-2019 GuaHao.com Corporation Limited. All rights reserved.
+ * This software is the confidential and proprietary information of GuaHao Company.
+ * ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only
+ * in accordance with the terms of the license agreement you entered into with GuaHao.com.
+ */
+package com.imooc.order.service.impl;
+
+import com.imooc.order.dataobject.OrderDetail;
+import com.imooc.order.dataobject.OrderMaster;
+import com.imooc.order.dto.OrderDTO;
+import com.imooc.order.enums.OrderStatusEnum;
+import com.imooc.order.enums.PayStatusEnum;
+import com.imooc.order.repository.OrderDetailRepository;
+import com.imooc.order.repository.OrderMasterRepository;
+import com.imooc.order.service.OrderService;
+import com.imooc.order.utils.KeyUtil;
+import com.imooc.product.client.ProductClient;
+import com.imooc.product.common.DecreaseStockInput;
+import com.imooc.product.common.ProductInfoOutput;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * TODO
+ *
+ * @author gaoty
+ * @version V1.0
+ * @since 2019-02-23 13:38
+ */
+@Service
+public class OrderServiceImpl implements OrderService {
+
+    @Autowired
+    private ProductClient productClient;
+
+    @Autowired
+    private OrderDetailRepository orderDetailRepository;
+
+    @Autowired
+    private OrderMasterRepository orderMasterRepository;
+
+    @Override
+    public OrderDTO create(OrderDTO orderDTO) {
+        String orderId = KeyUtil.genUniqueKey();
+
+        //查询商品信息(调用商品服务)
+        List<String> productIdList = orderDTO.getOrderDetailList().stream()
+            .map(OrderDetail::getProductId)
+            .collect(Collectors.toList());
+        List<ProductInfoOutput> productInfoList = productClient.listForOrder(productIdList);
+
+        //计算总价
+        BigDecimal orderAmout = new BigDecimal(BigInteger.ZERO);
+        for (OrderDetail orderDetail: orderDTO.getOrderDetailList()) {
+            for (ProductInfoOutput productInfo: productInfoList) {
+                if (productInfo.getProductId().equals(orderDetail.getProductId())) {
+                    //单价*数量
+                    orderAmout = productInfo.getProductPrice()
+                        .multiply(new BigDecimal(orderDetail.getProductQuantity()))
+                        .add(orderAmout);
+                    BeanUtils.copyProperties(productInfo, orderDetail);
+                    orderDetail.setOrderId(orderId);
+                    orderDetail.setDetailId(KeyUtil.genUniqueKey());
+                    //订单详情入库
+                    orderDetailRepository.save(orderDetail);
+                }
+            }
+        }
+
+        //扣库存(调用商品服务)
+        List<DecreaseStockInput> decreaseStockInputList = orderDTO.getOrderDetailList().stream()
+            .map(e -> new DecreaseStockInput(e.getProductId(), e.getProductQuantity()))
+            .collect(Collectors.toList());
+        productClient.decreaseStock(decreaseStockInputList);
+
+        //订单入库
+        OrderMaster orderMaster = new OrderMaster();
+        orderDTO.setOrderId(orderId);
+        BeanUtils.copyProperties(orderDTO, orderMaster);
+        orderMaster.setOrderAmount(orderAmout);
+        orderMaster.setOrderStatus(OrderStatusEnum.NEW.getCode());
+        orderMaster.setPayStatus(PayStatusEnum.WAIT.getCode());
+        orderMasterRepository.save(orderMaster);
+        return orderDTO;
+    }
+}
